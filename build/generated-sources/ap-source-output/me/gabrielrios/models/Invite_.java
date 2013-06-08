08f2b924204bb3ed5369ef021320ea4e24a05eda
Gabriel Rios <gabrielfalcaorios@gmail.com>
2013-06-08 10:31:21 -0300

    Login :D

--- build/generated-sources/ap-source-output/me/gabrielrios/models/Invite_.java
+++ build/generated-sources/ap-source-output/me/gabrielrios/models/Invite_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import me.gabrielrios.models.Party;
 import me.gabrielrios.models.User;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-06-08T09:42:48")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-06-08T10:27:50")
 @StaticMetamodel(Invite.class)
 public class Invite_ { 
 
--- build/generated-sources/ap-source-output/me/gabrielrios/models/User_.java
+++ build/generated-sources/ap-source-output/me/gabrielrios/models/User_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import me.gabrielrios.models.Invite;
 import me.gabrielrios.models.Party;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-06-08T09:42:48")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-06-08T10:27:50")
 @StaticMetamodel(User.class)
 public class User_ { 
 
